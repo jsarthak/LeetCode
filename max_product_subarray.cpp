c4c24a5e909a8eaffced5f798768410c16193985
Sarthak Jain <jsarthak@pop-os.localdomain>
2021-07-09 09:56:18 +0530

    max product subarray added

--- max_product_subarray.cpp
+++ max_product_subarray.cpp
@@ -3,21 +3,28 @@ using namespace std;
 
 int maxProduct(vector<int>& nums){
 
+	// find the size of the array
 	int n = nums.size();
+
+	// if array contains a signle element return it
 	if (n == 1) return nums[0];
 
+	// variables intitialization
 	int minP = nums[0];
 	int maxP = nums[0];
 	int ans = nums[0];
 
+	// loop through each number in the array
 	for (int i = 1; i < n; i++){
 		
+		// if a number is negative then swap min and max
 		if (nums[i] < 0){
 			int temp = minP;
 			minP = maxP;
 			maxP = temp;
 		}
 
+		// calculate new min and max
 		minP = min(nums[i], nums[i]*minP);
 		maxP = max(nums[i], nums[i]*maxP);
 		ans = max(ans, maxP);
